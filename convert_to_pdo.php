f7580835554dfb9e6b96f53f3a8566f9434bbaee
VM-DEV <development@planet-itservices.com>
2019-09-12 17:19:59 +0200

    init

---
+++ convert_to_pdo.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Ein Script mit dem man non PDO Scripte ins PDO umwandeln kann.
+ * Es schaut ob in einer Query Variablen drin sind und ersetzt diese durch Platzhalter
+ */
+
+
+$input_file = __DIR__ . '/nopdo.txt';
+
+// Regex: https://regex101.com/r/bErI90/2/
+
+
